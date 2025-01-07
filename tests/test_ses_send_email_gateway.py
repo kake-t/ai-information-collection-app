4efbb093ef77f56c67c9d9037908986e0c8abe40
kake-t <36560265+kake-t@users.noreply.github.com>
2025-01-07 12:37:57 +0000

    エラー発生時のテストを追加

--- tests/test_ses_send_email_gateway.py
+++ tests/test_ses_send_email_gateway.py
@@ -34,3 +34,21 @@ def test_send_email_success(gateway):
 
     # 検証
     assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+
+@mock_aws
+def test_send_email_error(gateway):
+    # 準備
+    source = "invalid_email"  # 不正なメールアドレス
+    destination = "destination@example.com"
+    subject = "test title"
+    body = "test body"
+
+    request = SendEmailRequest(
+        source=source, destination=destination, subject=subject, body=body
+    )
+
+    # 実行 & 検証
+    with pytest.raises(Exception) as exc_info:
+        gateway.send_email(request)
+    assert "SES send mail error" in str(exc_info.value)
