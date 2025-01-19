2916dcb4983f953afa50c6d12acb34546e4ca536
kake-t <36560265+kake-t@users.noreply.github.com>
2025-01-19 04:33:32 +0000

    ニュースのタイトルを本日日付に修正

--- src/domain/entities/send_email.py
+++ src/domain/entities/send_email.py
@@ -1,9 +1,13 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 
 @dataclass(frozen=True)
 class SendEmailRequest:
     source: str
     destination: str
-    subject: str
     body: str
+    subject: str = field(
+        default_factory=lambda: f"{datetime.now(ZoneInfo('Asia/Tokyo')).strftime('%Y-%m-%d')}のAIニュースです。"
+    )
