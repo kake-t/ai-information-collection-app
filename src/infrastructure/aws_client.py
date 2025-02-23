fb11a18adae690f3a372431e921fda9866fae379
kake-t <36560265+kake-t@users.noreply.github.com>
2025-02-23 03:40:39 +0000

    ANN401を対象外に追加

--- src/infrastructure/aws_client.py
+++ src/infrastructure/aws_client.py
@@ -3,7 +3,7 @@ from typing import Any
 import boto3
 
 
-def get_ses_client(region: str) -> Any:  # noqa: ANN401
+def get_ses_client(region: str) -> Any:
     return boto3.client(
         "ses",
         region_name=region,
